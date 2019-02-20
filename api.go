4c464d21eb8c60437a0da17853170041ba8aaded
Song Yang <ysong@digital-transaction.com>
2019-02-20 13:10:34 +0800

    PCO-213 Added OpenAnyByToken() and OpenManyByToken().

--- api.go
+++ api.go
@@ -1,5 +1,5 @@
 //
-// Copyright 2018 Digital Transaction Limited. All Rights Reserved.
+// Copyright 2019 Digital Transaction Limited. All Rights Reserved.
 //
 // Authors: Yang SONG, Eric Ma
 //
@@ -151,6 +151,40 @@ func OpenAny(endpointSpecs string, clientId string, credential string, certPath 
 }
 
 // certPath: check OpenAny usage
+// expireTimestamp: could be set to -1, if user do not want to track when to renew the client
+
+func OpenAnyByToken(endpointSpecs string, token string, expireTimestamp int64, certPath string) (*Client, error) {
+	randSrc := rand.NewSource(time.Now().UnixNano())
+	randGen := rand.New(randSrc)
+
+	// do until endpoints is empty:
+	//  randomly select endpoint from endpoints
+	//  try to openOne(endpoint) -> c
+	//  if ok
+	//    return c
+	//  remove endpoint from endpoints
+	// return error
+
+	var lastError error
+	endpoints := strings.Split(endpointSpecs, " ")
+	for len(endpoints) != 0 {
+		randPick := randGen.Intn(len(endpoints))
+		endpoint := endpoints[randPick]
+
+		client, err := openOne(endpoint, certPath, token)
+		if err != nil {
+			return nil, fmt.Errorf("CLIENT: Failed to open a connection using token. %v", err)
+		}
+		client.expireTimestamp = expireTimestamp
+		client.endpointSpecs = endpointSpecs
+
+		return client, nil
+	}
+
+	return nil, fmt.Errorf("CLIENT: Failed to open any client using token. Last error: %v", lastError)
+}
+
+// certPath: check OpenAny usage
 
 func OpenMany(endpointSpecs string, clientId string, credential string, certPath string) ([]*Client, error) {
 	endpoints := strings.Split(endpointSpecs, " ")
@@ -199,6 +233,32 @@ func OpenMany(endpointSpecs string, clientId string, credential string, certPath
 	if len(clients) == 0 {
 		// All attemps to connect are failed.
 		return nil, fmt.Errorf("CLIENT: Failed to open all clients. Last error: %v", lastError)
+	} else {
+		if lastError.Error() == "" {
+			return clients, nil
+		}
+		// All good or some attemps to connect failed. Opened all clients or opened some clients.
+		return clients, lastError
+	}
+}
+
+func OpenManyByToken(endpointSpecs string, token string, expireTimestamp int64, certPath string) ([]*Client, error) {
+	endpoints := strings.Split(endpointSpecs, " ")
+	lastError := fmt.Errorf("")
+	clients := make([]*Client, 0)
+	for _, endpoint := range endpoints {
+		client, err := openOne(endpoint, certPath, token)
+		if err != nil {
+			lastError = fmt.Errorf("%v%v\n", lastError, err)
+			continue
+		}
+		client.expireTimestamp = expireTimestamp
+		client.endpointSpecs = endpointSpecs
+		clients = append(clients, client)
+	}
+	if len(clients) == 0 {
+		// All attemps to connect are failed.
+		return nil, fmt.Errorf("CLIENT: Failed to open all clients using token. Last error: %v", lastError)
 	} else {
 		if lastError.Error() == "" {
 			return clients, nil
